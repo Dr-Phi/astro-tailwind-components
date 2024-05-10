401b8cbf2eb8158b714e1ac44ad9536bd91d75bb
Elias Rojas <eliasrojaslon@gmail.com>
2024-05-10 13:28:56 -0500

    HTML for Landing Page done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,14 @@ export default {
       colors: {
         "astro-red": "#df376e",
         "astro-pink": "#ee40ed",
+        "clr-cyan": "hsl(171, 66%, 44%)",
+        "clr-light-blue": "hsl(233, 100%, 69%)",
+        "clr-grayish": "hsl(210, 10%, 33%)",
+        "clr-light-gray": "hsl(201, 11%, 66%)",
+      },
+      backgroundImage: {
+        "landing-hm": "url('src/images/landing/bg-header-mobile.png')",
+        "landing-hd": "url('src/images/landing/bg-header-desktop.png')",
       },
     },
   },
