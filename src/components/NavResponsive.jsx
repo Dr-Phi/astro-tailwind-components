af7ddfc8d2fd5f080a5dcc026ac68cf8c3d07272
Elias Rojas <eliasrojaslon@gmail.com>
2024-05-22 15:14:05 -0500

    Desktop md screens done, pending find out on lg

--- src/components/NavResponsive.jsx
+++ src/components/NavResponsive.jsx
@@ -24,20 +24,26 @@ const NavResponsive = () => {
         </span>
       </div>
       <div
-        className={`bg-[#2b272f] md:bg-transparent w-full h-[94vh] md:h-10 absolute top-[-96vh] md:static bg-no-repeat bg-bottom bg-contain bg-insurance-nav transition-all z-30 ${isOpen && "top-[6rem]"}`}
+        className={`bg-[#2b272f] md:bg-transparent w-full h-[94vh] md:h-10 absolute top-[-96vh] md:static bg-no-repeat bg-bottom bg-contain max-md:bg-insurance-nav transition-all z-30 ${isOpen && "top-[6rem]"}`}
       >
-        <ul className="ff-karla text-white font-semibold tracking-widest flex flex-col gap-6 text-center pt-9 md:text-[#837d87] md:flex-row items-center md:gap-5 md:pt-0 md:justify-end">
+        <ul className="ff-karla text-white font-semibold tracking-widest flex flex-col gap-6 text-center pt-9 md:text-[#837d87] md:flex-row items-center md:gap-5 md:pt-0 md:justify-end lg:gap-9">
           <li>
-            <a href="#">HOW WE WORK</a>
+            <a href="#" className="md:hover:text-[#2c2640] md:hover:font-bold">
+              HOW WE WORK
+            </a>
           </li>
           <li>
-            <a href="#">BLOG</a>
+            <a href="#" className="md:hover:text-[#2c2640] md:hover:font-bold">
+              BLOG
+            </a>
           </li>
           <li>
-            <a href="#">ACCOUNT</a>
+            <a href="#" className="md:hover:text-[#2c2640] md:hover:font-bold">
+              ACCOUNT
+            </a>
           </li>
           <a href="#">
-            <button className="md:text-slate-950 border-2 md:border-slate-950 px-24 py-2 md:px-5 md:me-4">
+            <button className="border-2 px-24 py-2 hover:bg-slate-50 hover:text-[#2c2640] md:px-5 md:me-4 md:text-[#2c2640] md:border-[#2c2640] md:hover:bg-[#2c2640] md:hover:text-white">
               VIEW PLANS
             </button>
           </a>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,12 +10,31 @@ export default {
         "clr-light-blue": "hsl(233, 100%, 69%)",
         grayish: "#4c545d",
         "clr-light-gray": "hsl(201, 11%, 66%)",
+        darkBlue: "#2c2640",
       },
       backgroundImage: {
         "landing-hm": "url('src/images/landing/bg-header-mobile.png')",
         "landing-hd": "url('src/images/landing/bg-header-desktop.png')",
         "insurance-nav":
           'url("src/images/insurance/bg-pattern-mobile-nav.svg")',
+        "insurance-imgm": 'url("src/images/insurance/image-intro-mobile.jpg")',
+        "insurance-imgd": 'url("src/images/insurance/image-intro-desktop.jpg")',
+        "insurance-introlm":
+          'url("src/images/insurance/bg-pattern-intro-left-mobile.svg")',
+        "insurance-introrm":
+          'url("src/images/insurance/bg-pattern-intro-right-mobile.svg")',
+        "insurance-introld":
+          'url("src/images/insurance/bg-pattern-intro-left-desktop.svg")',
+        "insurance-intrord":
+          'url("src/images/insurance/bg-pattern-intro-right-desktop.svg")',
+        "insurance-howm":
+          'url("src/images/insurance/bg-pattern-how-we-work-mobile.svg")',
+        "insurance-howd":
+          'url("src/images/insurance/bg-pattern-how-we-work-desktop.svg")',
+        "insurance-footerm":
+          'url("src/images/insurance/bg-pattern-footer-mobile.svg")',
+        "insurance-footerd":
+          'url("src/images/insurance/bg-pattern-footer-desktop.svg")',
       },
       fontFamily: {
         karla: ["Karla", "sans-serif"],
